48828d7e00031ae51be0bfa595de7143a3faa0ab
AyeNyeinSan <aye2904@gmail.com>
2025-05-24 13:06:27 +0700

    Add: Footer component

--- src/pages/navbarMenu/navbarmenu.tsx
+++ src/pages/navbarMenu/navbarmenu.tsx
@@ -20,8 +20,8 @@ export default function NavBarMenuPage() {
        },
      ];
     return (
-        <>
+
           <NavBar logoText="MyanSen" navItems={navItems} />
-        </>
+    
     );
 }
